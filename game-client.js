ee825d23fee9507d4edaa9d23d8261c17df67724
NeTook52 <kiber.chpuck228@gmail.com>
2025-12-24 02:41:45 +1000

    Update game-client.js

--- game-client.js
+++ game-client.js
@@ -11,13 +11,13 @@ if (args.length !== 2) {
 const min = parseInt(args[0]);
 const max = parseInt(args[1]);
 
-// Загадываем случайное число
+
 const secretNumber = Math.floor(Math.random() * (max - min + 1)) + min;
 console.log('Загадано число:', secretNumber);
 
-// Подключаемся к серверу
+
 const socket = net.createConnection({ port: 5555, host: '127.0.0.1' }, function() {
-    // Отправляем диапазон серверу
+   
     const rangeMessage = { range: `${min}-${max}` };
     console.log('Отправляю диапазон:', JSON.stringify(rangeMessage));
     socket.write(JSON.stringify(rangeMessage) + '\n');
@@ -25,11 +25,11 @@ const socket = net.createConnection({ port: 5555, host: '127.0.0.1' }, function(
 
 let buffer = '';
 
-// Обработка ответов от сервера
+
 socket.on('data', function(data) {
     buffer += data.toString();
     
-    // Обрабатываем все полные JSON сообщения
+    
     let newlineIndex;
     while ((newlineIndex = buffer.indexOf('\n')) !== -1) {
         const message = buffer.substring(0, newlineIndex);
@@ -38,24 +38,24 @@ socket.on('data', function(data) {
         try {
             const jsonData = JSON.parse(message);
             
-            // Логируем ответ сервера
+           
             console.log('Ответ сервера:', JSON.stringify(jsonData));
             
             const serverGuess = jsonData.answer;
             
-            // Проверяем ответ сервера
+       
             if (serverGuess < secretNumber) {
-                // Число больше
+               
                 const hint = { hint: 'more' };
                 console.log('Отправляю подсказку:', JSON.stringify(hint));
                 socket.write(JSON.stringify(hint) + '\n');
             } else if (serverGuess > secretNumber) {
-                // Число меньше
+               
                 const hint = { hint: 'less' };
                 console.log('Отправляю подсказку:', JSON.stringify(hint));
                 socket.write(JSON.stringify(hint) + '\n');
             } else {
-                // Угадал!
+                
                 console.log('Сервер угадал число! Игра окончена.');
                 socket.end();
                 process.exit(0);
