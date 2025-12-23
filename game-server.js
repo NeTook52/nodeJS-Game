ba3647e80d5b4c04a4356aaf0414c50bc0971782
NeTook52 <kiber.chpuck228@gmail.com>
2025-12-24 02:43:17 +1000

    Update game-server.js

--- game-server.js
+++ game-server.js
@@ -1,6 +1,6 @@
 const net = require('net');
 
-// Выводим сообщение о готовности
+
 console.log('Готов к игре...');
 
 const server = net.createServer(function(socket) {
@@ -8,11 +8,11 @@ const server = net.createServer(function(socket) {
     let max = 0;
     let buffer = '';
     
-    // Обработка входящих данных
+    
     socket.on('data', function(data) {
         buffer += data.toString();
         
-        // Обрабатываем все полные JSON сообщения
+        
         let newlineIndex;
         while ((newlineIndex = buffer.indexOf('\n')) !== -1) {
             const message = buffer.substring(0, newlineIndex);
@@ -21,32 +21,32 @@ const server = net.createServer(function(socket) {
             try {
                 const jsonData = JSON.parse(message);
                 
-                // Логируем сообщение от клиента
+                
                 console.log('Получено от клиента:', JSON.stringify(jsonData));
                 
-                // Если получили диапазон, начинаем игру
+                
                 if (jsonData.range) {
                     const rangeParts = jsonData.range.split('-');
                     min = parseInt(rangeParts[0]);
                     max = parseInt(rangeParts[1]);
                     
-                    // Первая попытка - берем середину диапазона
+                    
                     const guess = Math.floor((min + max) / 2);
                     const response = { answer: guess };
                     console.log('Отправляю:', JSON.stringify(response));
                     socket.write(JSON.stringify(response) + '\n');
                 }
-                // Если получили подсказку
+                
                 else if (jsonData.hint) {
                     if (jsonData.hint === 'more') {
-                        // Число больше, увеличиваем минимум
+                        
                         min = Math.floor((min + max) / 2) + 1;
                     } else if (jsonData.hint === 'less') {
-                        // Число меньше, уменьшаем максимум
+                       
                         max = Math.floor((min + max) / 2) - 1;
                     }
                     
-                    // Вычисляем новую попытку
+                   
                     const guess = Math.floor((min + max) / 2);
                     const response = { answer: guess };
                     console.log('Отправляю:', JSON.stringify(response));
@@ -63,7 +63,7 @@ const server = net.createServer(function(socket) {
     });
 });
 
-// Слушаем на порту 5555
+
 server.listen(5555, '127.0.0.1', function() {
     console.log('Сервер запущен на порту 5555');
 });
